7bf6b6aaea6abdcd3d2ce3652fb0a0a76a250ccd
joaoalrodrigues <jalxnd@gmail.com>
2020-01-22 16:28:34 +0100

    Updates

---
+++ precache-manifest.5e53c61ac5cd4cb088048cfc30a2d22f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "505e1cc8b53a1dd0fba4ac915858bb00",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "2883c917386f884875dc",
+    "url": "/monsters-rolodex/static/css/main.f267c22f.chunk.css"
+  },
+  {
+    "revision": "c68e14fdc7e1ba4a5621",
+    "url": "/monsters-rolodex/static/js/2.9e7d0785.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.9e7d0785.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2883c917386f884875dc",
+    "url": "/monsters-rolodex/static/js/main.e0009ec5.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
